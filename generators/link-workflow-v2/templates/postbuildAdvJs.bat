0c809e2e3276fdb89d2a97acc57e44b872f8a1ca
Luca Nodari <l.nodari@abletech.it>
2021-07-08 17:49:32 +0200

    Modifiche alla sln javascript e postbuild.bat

--- generators/link-workflow-v2/templates/postbuildAdvJs.bat
+++ generators/link-workflow-v2/templates/postbuildAdvJs.bat
@@ -2,5 +2,5 @@ ECHO "Building Plugin"
 
 SET TargetDir=%1
 IF NOT EXIST "%TargetDir%\client" MKDIR "%TargetDir%\client"
-XCOPY "..\scripts\build\" "%TargetDir%\client" /y /s
+XCOPY "..\scripts\src" "%TargetDir%\client" /y /s
 powershell compress-archive -Path "%TargetDir%\*" -Update -DestinationPath "..\<%= props.pluginname %>"
