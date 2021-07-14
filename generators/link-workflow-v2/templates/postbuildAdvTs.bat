9a081cfe913965dc496b497cc1c7ca2520b87513
Luca Nodari <l.nodari@abletech.it>
2021-07-14 16:34:06 +0200

    aggiornamento package 0.1.8

--- generators/link-workflow-v2/templates/postbuildAdvTs.bat
+++ generators/link-workflow-v2/templates/postbuildAdvTs.bat
@@ -2,7 +2,7 @@ ECHO "Building Plugin"
 
 SET TargetDir=%1
 IF NOT EXIST "%TargetDir%\client" MKDIR "%TargetDir%\client"
-XCOPY "..\scripts\build\" "%TargetDir%\client" /y /s
+XCOPY "..\scripts\build" "%TargetDir%\client" /y /s
 powershell compress-archive -Path "%TargetDir%\*" -Update -DestinationPath "..\<%= props.pluginname %>"
 
 
